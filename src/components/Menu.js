65ddb95db4ed30fb6f1f6ed6a5f08a46c61f5200
Eunwoo <dmsdn960@gmail.com>
2019-02-17 13:23:04 +0900

    menu style

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import './Menu.css';
 import { NavLink } from 'react-router-dom';
 
 const Menu = () => {
     return(
         <div>
-            <ul>
-                <li><NavLink to="/yena">오예나</NavLink></li>
-                <li><NavLink to="/jihye">서지혜</NavLink></li>
-                <li><NavLink to="/jungmin">류정민</NavLink></li>
-                <li><NavLink to="/junsik">오준식</NavLink></li>
-                <li><NavLink to="/sungmin">김성민</NavLink></li>
+            <ul className='menuList'>
+                <li className="menuItem"><NavLink to="/yena">오예나</NavLink></li>
+                <li className="menuItem"><NavLink to="/jihye">서지혜</NavLink></li>
+                <li className="menuItem"><NavLink to="/jungmin">류정민</NavLink></li>
+                <li className="menuItem"><NavLink to="/junsik">오준식</NavLink></li>
+                <li className="menuItem"><NavLink to="/sungmin">김성민</NavLink></li>
             </ul>
         </div>
     );
