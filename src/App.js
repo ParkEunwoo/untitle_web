9136f79cec97f29fec6f1b52ee152875804b727e
SeoJihye <mansesjh22@naver.com>
2019-03-01 13:39:46 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Menu from './components/Menu';
-import { Route,  } from 'react-router-dom';
+import { Route  } from 'react-router-dom';
 import {
     InputActivity,
     NoticeBoard,
