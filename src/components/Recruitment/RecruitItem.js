63e62f416c35ace33bfc3a48e273d20a87c517b0
Eunwoo <dmsdn960@gmail.com>
2019-03-02 21:30:53 +0900

    recruit

--- src/components/Recruitment/RecruitItem.js
+++ src/components/Recruitment/RecruitItem.js
@@ -3,16 +3,14 @@ import React, { Component } from 'react';
 class RecruitItem extends Component {    
     render() {
       return (
-        <div className = "recruitList">
-            <div className = "kind">{this.props.type}</div>
-            <div className = "name">
-                <h2>{/*과목명*/}{this.props.title}</h2>
-                <h3>{/*팀장명*/}{this.props.leader}</h3>
-            </div>
-            {/*활동기간-모집기간보다 글자 크게*/}
-            <div>{/*활동기간*/}{this.props.asDate} ~ {this.props.aeDate}</div>
-            <div>모집기간:{this.props.rsDate} ~ {this.props.reDate} </div>
-            <div>{/*모집인원*/}{this.props.jNum}/{this.props.rNum}명</div>
+        <div>
+          <h4>{this.props.type}</h4>
+          <h2>{this.props.title}</h2>
+          <h3>{this.props.leader}</h3>
+          <p>
+            <span>{this.props.startDate} ~ {this.props.endDate}</span>
+            <span>{this.props.joinNum}/{this.props.recruitNum}명</span>
+          </p>
         </div>
       );
     }
--- src/components/Recruitment/RecruitList.js
+++ src/components/Recruitment/RecruitList.js
@@ -6,46 +6,48 @@ const recruit = [
   {
     _id: 0,
     type: '멘토링',
-    title: '과목명',
-    leader: '팀장명',
-    asDate:'2019-03-15',
-    aeDate:'2019-06-21',
-    rsDate:'2019-03-04',
-    reDate:'2019-03-14',
-    jNum: 8,
-    rNum: 20
+    title: '1학년을 위한 프로그래밍',
+    leader: '류정민',
+    period: {
+      startDate:'2019-03-11',
+      endDate:'2019-06-21'
+    },
+    recruitNum: 20,
+    joinNum: 0
   },
   {
     _id: 1,
-    type: '스터디',
-    title: '과목명',
-    leader: '팀장명',
-    asDate:'2019-03-10',
-    aeDate:'2019-04-30',
-    rsDate:'2019-03-04',
-    reDate:'2019-03-09',
-    jNum: 3,
-    rNum: 10
+    type: '멘토링',
+    title: '웹 프론트앤드',
+    leader: '박은우',
+    period: {
+      startDate:'2019-03-11',
+      endDate:'2019-06-21'
+    },
+    recruitNum: 20,
+    joinNum: 0
   },
   {
     _id: 2,
-    type: '프로젝트',
-    title: '과목명',
-    leader: '팀장명',
-    asDate:'2019-03-20',
-    aeDate:'2019-08-30',
-    rsDate:'2019-03-04',
-    reDate:'2019-03-09',
-    jNum: 1,
-    rNum: 10
-  }
+    type: '멘토링',
+    title: '디자인을 위한 얕고 넓은 멘토링',
+    leader: '김윤회',
+    period: {
+      startDate:'2019-03-11',
+      endDate:'2019-06-21'
+    },
+    recruitNum: 20,
+    joinNum: 0
+  },
 ]
 
 class RecruitList extends Component {
   render() {
-    const recruitment =recruit.map(info => <RecruitItem key = {info._id} title = {info.title} type = {info.type} leader = {info.leader} 
-      asDate= {info.asDate} aeDate= {info.aeDate} rsDate= {info.rsDate} reDate= {info.reDate}
-      jNum = {info.jNum} rNum = {info.rNum}  />);
+    const recruitment =recruit.map(info => <RecruitItem key = {info._id} title = {info.title}
+      type = {info.type}
+      leader = {info.leader} 
+      startDate = {info.period.startDate} endDate= {info.period.endDate}
+      recruitNum = {info.recruitNum} joinNum = {info.joinNum}  />);
     
     return (
       <div>
