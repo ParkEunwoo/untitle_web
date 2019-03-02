f084fc4d630acd1f7b856c5853588b7986bfa767
SeoJihye <mansesjh22@naver.com>
2019-03-02 22:46:00 +0900

    header 버튼

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,12 +9,28 @@ const Contents = styled.div `
     padding-bottom : 5em;
 `;
 
+const Button = styled.button `
+    font-size : 1em;
+    border-radius: 30px;
+    border: 2px solid #ecf0f1;
+    background-color: rgba(0,0,0,0);
+    color : rgba(255,255,255,100);
+    cursor: pointer;
+    padding-left: 2em;
+    padding-right: 2em;
+    padding-top: 0.5em;
+    padding-bottom: 0.5em;
+    &:focus {
+    outline: none;
+    }
+`;
+
 const Header = (props) => {
     return (
         <Contents>
             <h1>{props.title}</h1>
             {!!props.sub && <h2>{props.sub}</h2>}
-            {!!props.register && <button>{props.register}</button>}
+            {!!props.register && <Button>{props.register}</Button>}
         </Contents>
     )
 }
