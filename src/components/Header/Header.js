21ddb4f97347e003500bc09e91e7261ad810aa6b
Eunwoo <dmsdn960@gmail.com>
2019-03-02 20:59:17 +0900

    console

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,6 @@ const Header = (props) => {
     return (
         <div>
             <h1>{props.title}</h1>
-            {console.log(true&&'a')}
             {!!props.sub && <h2>{props.sub}</h2>}
             {!!props.register && <button>{props.register}</button>}
         </div>
