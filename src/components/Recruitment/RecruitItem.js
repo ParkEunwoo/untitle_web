6a1b626ade5d98d03faeb62594bbbb3e308a18b6
SeoJihye <mansesjh22@naver.com>
2019-03-03 16:14:36 +0900

    recruit

--- src/components/Recruitment/RecruitItem.js
+++ src/components/Recruitment/RecruitItem.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-const Box = styled.div `
-  display : flex;
-  flex-direction : row ;
+const Item = styled.div ` 
+  box-shadow: 1em 1em 1em rgba(0, 0, 0, 0.2); 
+  padding: 1.5em;
+  text-align: center;
+  background-color: #f8f8f8;
+  margin-top : 1em;
+  margin-bottom: 1.5em;
+  margin-left : 0.5em;
+  margin-right : 0.5em;
+  
 `;
-/*
-const Item = styled.div `
-  display : flex;
-  flex-direction : column;
-`;*/
 
 class RecruitItem extends Component {    
     render() {
       return (
-        <Box>
-          <div>
+          <Item className="Item">
             <h4>{this.props.type}</h4>
             <h2>{this.props.title}</h2>
             <h3>{this.props.leader}</h3>
@@ -23,8 +24,8 @@ class RecruitItem extends Component {
               <span>{this.props.startDate} ~ {this.props.endDate}</span>
               <span>{this.props.joinNum}/{this.props.recruitNum}명</span>
             </p>
-          </div>
-        </Box>
+          </Item>
+        
 
       );
     }
